8cdb053b10a386c4777b858a10f9610a35f612de
TheUnknownHacker <128781393+The-UnknownHacker@users.noreply.github.com>
2024-06-29 12:47:19 +1000

    Update

--- src/server/index.ts
+++
@@ -1,8 +0,0 @@
-import { helloRouter } from "@/server/routers/hello";
-import { router } from "@/server/trpc";
-
-export const appRouter = router({
-  hello: helloRouter,
-});
-
-export type AppRouter = typeof appRouter;
--- src/server/routers/hello.ts
+++
@@ -1,6 +0,0 @@
-import { publicProcedure } from "@/server/trpc";
-
-export const helloRouter = publicProcedure.query(() => {
-  console.count("helloRouter called");
-  return new Date().toISOString();
-});
