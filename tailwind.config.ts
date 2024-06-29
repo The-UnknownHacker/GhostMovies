e75d60d4fb2c93eeca01ef6cd8e7366195bfcd89
TheUnknownHacker <128781393+The-UnknownHacker@users.noreply.github.com>
2024-06-29 12:45:53 +1000

    Update v2

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,100 +1,50 @@
-import { fontFamily } from "tailwindcss/defaultTheme";
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  darkMode: ["class"],
-  content: [
-    "./pages/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
-    "./app/**/*.{ts,tsx}",
-    "./src/**/*.{ts,tsx}",
-  ],
+import { allThemes, defaultTheme, safeThemeList } from "./themes";
+import type { Config } from "tailwindcss";
+import plugin from "tailwindcss/plugin";
+
+const themer = require("tailwindcss-themer");
+
+const config: Config = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  safelist: safeThemeList,
   theme: {
-    screens: {
-      xs: "500px",
-      // => @media (min-width: 500px) { ... }
-
-      sm: "640px",
-      // => @media (min-width: 640px) { ... }
-
-      md: "768px",
-      // => @media (min-width: 768px) { ... }
-
-      lg: "1024px",
-      // => @media (min-width: 1024px) { ... }
-
-      xl: "1280px",
-      // => @media (min-width: 1280px) { ... }
-
-      "2xl": "1400px",
-      // => @media (min-width: 1536px) { ... }
-    },
-    container: {
-      center: true,
-      padding: "4vw",
-      screens: {
-        "2xl": "1400px",
-      },
-    },
     extend: {
-      colors: {
-        border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
-        primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
-        },
-        secondary: {
-          DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
-        },
-        destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
-        },
-        muted: {
-          DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
-        },
-        accent: {
-          DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
-        },
-        popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
-        },
-        card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
-        },
+      /* breakpoints */
+      screens: {
+        ssm: "400px",
       },
-      borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
-      },
-      keyframes: {
-        "accordion-down": {
-          from: { height: 0 },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: 0 },
-        },
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-      },
+
+      /* fonts */
       fontFamily: {
-        sans: ["var(--font-sans)", ...fontFamily.sans],
-        heading: ["var(--font-heading)", ...fontFamily.sans],
+        "main": "'DM Sans'", // "main": "'Open Sans'",
       },
+
+      /* animations */
+      keyframes: {
+        "loading-pin": {
+          "0%, 40%, 100%": { height: "0.5em", "background-color": "#282336" },
+          "20%": { height: "1em", "background-color": "white" },
+        },
+      },
+      animation: { "loading-pin": "loading-pin 1.8s ease-in-out infinite" },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [
+    require("tailwind-scrollbar"),
+    themer({
+      defaultTheme: defaultTheme,
+      themes: [
+        {
+          name: "default",
+          ...defaultTheme,
+        },
+        ...allThemes,
+      ],
+    }),
+    plugin(({ addVariant }) => {
+      addVariant("dir-neutral", "[dir] &");
+    }),
+  ],
 };
+
+export default config;
---
+++ themes/all.ts
@@ -0,0 +1,13 @@
+import teal from "./list/teal";
+import blue from "./list/blue";
+import red from "./list/red";
+import gray from "./list/gray";
+import midnight from "./list/midnight";
+
+export const allThemes = [
+  teal,
+  blue,
+  gray,
+  red,
+  midnight
+]
